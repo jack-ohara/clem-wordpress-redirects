885decbb061bdd5efebe5cda0d904791aaea978e
Jack O'Hara <jack@jackohara.io>
2022-06-17 15:24:21 +0100

    First iteration. Logging the result to see what it looks like

---
+++ index.ts
@@ -0,0 +1,39 @@
+import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
+import axios from "axios";
+
+export default async function handler(event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> {
+    if (!process.env.WP_BASE_URL) {
+        return {
+            statusCode: 502,
+            body: JSON.stringify({ error: "Wordpress base URL not found" })
+        }
+    }
+
+    console.log(event)
+
+    if (!event.queryStringParameters || !event.queryStringParameters["path"]) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ error: "No path found in query string" })
+        }
+    }
+
+    const startingSitePath = event.queryStringParameters["path"]
+
+    const result = {
+        url: await getRedirectLocation(startingSitePath)
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(result)
+    }
+}
+
+async function getRedirectLocation(path: string): Promise<string> {
+    const response = await axios.get(path)
+
+    console.log(response)
+
+    return "";
+}
